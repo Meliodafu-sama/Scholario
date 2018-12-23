b1c0ba388ac95c44833aed05cb140492eb319f92
unidentifiedpy <unidentifiedpy@github.com>
2018-12-23 20:14:12 +0800

    Front end

--- resources/views/admin/individual.blade.php
+++ resources/views/admin/individual.blade.php
@@ -13,36 +13,23 @@ PUP-SFAS | Pending Sponsors
 
                 <div class="panel-body">
                     <h3>Your Blog Posts</h3>
-                    @if(count($posts) > 0)
                         <table class="table table-striped">
                             <tr>
                                 <th>Title</th>
                                 <th></th>
                                 <th></th>
                             </tr>
-                            @foreach($posts as $data)
+                            @foreach($data as $row)
                                 <tr>
-                                    <td>{{$data->campus_addr}}</td>
-                                    <td>
-                                        {!!Form::open(['action' => ['PostsController@destroy', $data->campus_name], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                            {{Form::hidden('_method', 'DELETE')}}
-                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                                        {!!Form::close()!!}
+                                    <td>{{$row->campus_addr}}</td>
+                                    <td>                                        
                                     </td>
                                 </tr>
                             @endforeach
                         </table>
-                    @else
-                        <p>You have no posts</p>
-                    @endif
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-	<!--foreach $data as $row{{$row -> campus_name}}
-	{{$row->campus_addr}}
-	@endforeach-->
-
 @endsection
