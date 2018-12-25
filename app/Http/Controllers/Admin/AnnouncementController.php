7751e666d15e098ab4d25e6a8f4a25dfb7813680
unidentifiedpy <unidentifiedpy@github.com>
2018-12-26 03:47:55 +0800

    insert update

--- app/Http/Controllers/Admin/AnnouncementController.php
+++ app/Http/Controllers/Admin/AnnouncementController.php
@@ -6,22 +6,23 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Announcements;
 use DB;
+use Log;
 
 class AnnouncementController extends Controller
 {
     public function addAnnounce(){
         return view('admin.addannouncement');
-    }
+    } 
 
     public function addAnnouncePost( Request $req ) {
+    	Log::debug( "Now posting ..." );
     	$title = $req->title;
-    	$author = $req->author;
+    	$author = "1";
     	$article = $req->article;
-    	$query = "insert into anc( anc_title, anc_author, anc_art, data ) values( \"$title\", \"$author\", \"$article\", now() )";
-    	print_r( $req );
-    	echo $query;
+        $query = "insert into anc( anc_title, anc_author, anc_art, date ) values( \"". $title ."\", \"". $author ."\", \"". $article ."\", now() )";
 
-    	DB::insert( $query );
+        DB::insert( $query );
+    	echo $query;
     	return redirect( "/admin/addannouncement" );
     }
     
